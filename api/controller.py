5ef3368d70fb169804f14429fb526ca667702e2c
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-02-20 14:02:27 -0300

    adding project router in the controller.py

--- api/controller.py
+++ api/controller.py
@@ -1,9 +1,15 @@
 from fastapi import APIRouter
 
+from .routes.project import router as project_router
 from .routes.collaborator import router as collaborator_router
 
 api_router = APIRouter()
 
+api_router.include_router(
+    project_router,
+    prefix="/projects",
+    tags=["Project"]
+)
 api_router.include_router(collaborator_router,
                           prefix="/collaboratos",
                           tags=["Collaborator"]
