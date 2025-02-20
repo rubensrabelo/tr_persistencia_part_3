2eb7700a96167406eed7343dd1103abcd9a23623
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-02-20 12:15:11 -0300

    Adding Task, StatusEnum and Project in the models.py

--- models.py
+++ models.py
@@ -1,7 +1,35 @@
-from odmantic import Model, Reference
+from odmantic import Model, EmbeddedModel
+from bson import ObjectId
+from enum import Enum
+from datetime import datetime, timezone
+from typing import Optional
 
 
 class Collaborator(Model):
     name: str
     email: str
     function: str
+
+
+class StatusEnum(str, Enum):
+    NOT_DONE = "Not done"
+    DOING = "Doing"
+    DONE = "Done"
+
+
+class Task(EmbeddedModel):
+    name: str
+    description: str
+    create_at: datetime = datetime.now(timezone.utc)
+    update_at: datetime = datetime.now(timezone.utc)
+    status: StatusEnum = StatusEnum.NOT_DONE
+    collaborator: Optional[list[ObjectId]] = None
+
+
+class Project(Model):
+    name: str
+    description: str
+    create_at: datetime = datetime.now(timezone.utc)
+    update_at: datetime = datetime.now(timezone.utc)
+    status: StatusEnum = StatusEnum.NOT_DONE
+    tasks: list[Task] = []
