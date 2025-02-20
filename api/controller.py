63ad21c0dab6f035613ec4ab66c0a4e48d40fb48
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-02-20 09:49:54 -0300

    Adding collaborator router in the controller.py

--- api/controller.py
+++ api/controller.py
@@ -1,3 +1,10 @@
 from fastapi import APIRouter
 
+from .routes.collaborator import router as collaborator_router
+
 api_router = APIRouter()
+
+api_router.include_router(collaborator_router,
+                          prefix="/collaboratos",
+                          tags=["Collaborator"]
+                          )
