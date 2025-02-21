55dca094857893c3ab2878a31519dc13d1aa8393
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-02-21 16:30:30 -0300

    Fixing models

--- models.py
+++ models.py
@@ -1,7 +1,5 @@
-from odmantic import Model, EmbeddedModel
-from bson import ObjectId
+from odmantic import Model
 from enum import Enum
-from typing import Optional
 from datetime import datetime, timezone
 
 
@@ -17,14 +15,13 @@ class StatusEnum(str, Enum):
     DONE = "Done"
 
 
-class Task(EmbeddedModel):
-    id: Optional[int] = None
+class Task(Model):
     name: str
     description: str
     created_at: datetime = datetime.now(timezone.utc)
     updated_at: datetime = datetime.now(timezone.utc)
     status: StatusEnum = StatusEnum.NOT_DONE
-    collaborators: list[ObjectId] = []
+    collaborators: list[Collaborator] = []
 
 
 class Project(Model):
