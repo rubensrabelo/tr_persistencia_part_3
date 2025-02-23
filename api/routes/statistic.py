f438478387abc47225a97d4001be637643d843c2
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-02-23 08:52:13 -0300

    Adding total_tasks_by_project method in the statistic router

--- api/routes/statistic.py
+++ api/routes/statistic.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from starlette import status
 
 from database import get_engine
-from database import Project
+from models import Project
 
 
 router = APIRouter()
@@ -17,3 +17,31 @@ async def total_projects() -> dict:
     return {
         "total projects": total_projects
     }
+
+
+@router.get("/total/tasks/by/project")
+async def total_tasks_by_project():
+    collection = engine.get_collection(Project)
+
+    pipeline = [
+        {
+            "$project": {
+                "project_name": "$name",
+                "total_tasks": {"$size": {"$ifNull": ["$tasks", []]}}
+            }
+        },
+        {
+            "$sort": {"total_tasks": -1}
+        }
+    ]
+
+    results = await collection.aggregate(pipeline).to_list(length=None)
+    
+    results = [
+        {
+            "project_name": result["project_name"],
+            "total_tasks": result["total_tasks"]
+        }
+        for result in results
+    ]
+    return results
