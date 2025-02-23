0be11d191764cb5566083f754eee59412b0eb505
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-02-23 08:05:53 -0300

    Renaming database from mydatabase to db_projects

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 client = AsyncIOMotorClient(DATABASE_URL)
-engine = AIOEngine(client=client, database="mydatabase")
+engine = AIOEngine(client=client, database="db_projects")
 
 
 def get_engine() -> AIOEngine:
