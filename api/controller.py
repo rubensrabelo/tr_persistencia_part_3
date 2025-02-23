da926998a9e127ab7f2fdd089d5c480dc9219c48
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-02-23 08:14:17 -0300

    Adding statistic router in the router controller

--- api/controller.py
+++ api/controller.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from .routes.project import router as project_router
 from .routes.task import router as task_router
 from .routes.collaborator import router as collaborator_router
+from .routes.statistic import router as statistic_router
 
 api_router = APIRouter()
 
@@ -21,3 +22,8 @@ api_router.include_router(
     prefix="/collaboratos",
     tags=["Collaborator"]
     )
+api_router.include_router(
+    statistic_router,
+    prefix="/statistic",
+    tags=["Statistic"]
+)
